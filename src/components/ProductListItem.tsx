86ce9085342ffc9ed069315207e0139a27b5e792
Oluwatodimu Adegoke <adegokeoluwatodimu@yahoo.com>
2024-03-16 20:12:51 -0500

    Flat list

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -10,7 +10,11 @@ type ProductListItemProps = {
 const ProductListItem = ({ product } : ProductListItemProps) =>  {
     return(
       <View style={styles.container}>
-        <Image source={{uri : product.image || defualtPizzaImage}} style = {styles.image}/>
+        <Image 
+          source={{uri : product.image || defualtPizzaImage}} 
+          style = {styles.image}
+          resizeMode='contain'
+          />
         <Text style = {styles.title}>{product.name}</Text>
         <Text style = {styles.price}>${product.price}</Text>
       </View>
@@ -21,9 +25,11 @@ export  default ProductListItem;
 
 const styles = StyleSheet.create({
     container: {
+      flex: 1,
       backgroundColor: 'white',
       padding: 10,
       borderRadius: 20,
+      maxWidth: "50%",
     },
     title:  {
       fontSize: 18,
